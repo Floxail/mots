bfba69b64209ff4ade00321518aaf2a652cab607
Floxail <FLoxail07@live.fr>
2026-07-20 18:59:58 +0200

    feat: export solved grids to the Case/enums-compatible shape

---
+++ grid_generator/exporter.js
@@ -0,0 +1,52 @@
+var enums = require('../game_files/enums');
+var Case = require('../game_files/case');
+
+var ARROW_RIGHT = 0;
+var ARROW_BOTTOM = 2;
+
+function findSlotStartingAt(slots, cellIndex, axis) {
+  for (var i = 0; i < slots.length; i++) {
+    if (slots[i].axis === axis && slots[i].cells[0] === cellIndex) return i;
+  }
+  return -1;
+}
+
+function exportGrid(skeleton, slots, assignment, dictionary) {
+  var nbLines = skeleton.nbLines, nbColumns = skeleton.nbColumns;
+
+  var cases = skeleton.types.map(function (type, idx) {
+    if (type === enums.CaseType.Letter) return new Case.LetterCase(idx, null);
+    return new Case.DescriptionCase(idx, 'a');
+  });
+
+  slots.forEach(function (slot, slotIdx) {
+    var word = assignment[slotIdx];
+    slot.cells.forEach(function (cellIndex, pos) {
+      cases[cellIndex].value = word[pos];
+    });
+  });
+
+  cases.forEach(function (cell, idx) {
+    if (cell.type !== enums.CaseType.Description) return;
+
+    var rightSlotIdx = findSlotStartingAt(slots, idx + 1, 'H');
+    var belowSlotIdx = findSlotStartingAt(slots, idx + nbLines, 'V');
+
+    var attached = [];
+    if (rightSlotIdx !== -1) attached.push({ direction: ARROW_RIGHT, word: assignment[rightSlotIdx] });
+    if (belowSlotIdx !== -1) attached.push({ direction: ARROW_BOTTOM, word: assignment[belowSlotIdx] });
+
+    cell.nbDesc = attached.length;
+    cell.desc = [];
+    cell.arrow = [];
+    attached.forEach(function (a) {
+      var defs = dictionary.definitionsByWord.get(a.word) || [];
+      cell.desc.push(defs.length > 0 ? defs[0] : '');
+      cell.arrow.push(a.direction);
+    });
+  });
+
+  return { nbLines: nbLines, nbColumns: nbColumns, nbWords: slots.length, cases: cases };
+}
+
+module.exports = { exportGrid: exportGrid };
---
+++ test/exporter.test.js
@@ -0,0 +1,37 @@
+var test = require('node:test');
+var assert = require('node:assert');
+var enums = require('../game_files/enums');
+var dictionary = require('../grid_generator/dictionary');
+var exporter = require('../grid_generator/exporter');
+
+test('exportGrid fills letters and attaches definitions/arrows on description cells', function () {
+  var D = enums.CaseType.Description, L = enums.CaseType.Letter;
+  // 3 wide x 3 tall:
+  // row0: D L L   (horizontal word starting at col1)
+  // row1: L .  .  (vertical word starting at col0, row1)
+  // row2: L .  .
+  var skeleton = { nbLines: 3, nbColumns: 3, types: [D, L, L, L, D, D, L, D, D] };
+  var slots = [
+    { axis: 'H', cells: [1, 2], length: 2, crossings: [] },
+    { axis: 'V', cells: [3, 6], length: 2, crossings: [] }
+  ];
+  var assignment = ['AB', 'XY'];
+  var dico = dictionary.buildDictionary([
+    { word: 'AB', definitions: ['Across def'] },
+    { word: 'XY', definitions: ['Down def'] }
+  ]);
+
+  var grid = exporter.exportGrid(skeleton, slots, assignment, dico);
+
+  assert.strictEqual(grid.nbLines, 3);
+  assert.strictEqual(grid.nbColumns, 3);
+  assert.strictEqual(grid.cases[1].value, 'A');
+  assert.strictEqual(grid.cases[2].value, 'B');
+  assert.strictEqual(grid.cases[3].value, 'X');
+  assert.strictEqual(grid.cases[6].value, 'Y');
+
+  var descCell = grid.cases[0];
+  assert.strictEqual(descCell.nbDesc, 2);
+  assert.deepStrictEqual(descCell.desc, ['Across def', 'Down def']);
+  assert.deepStrictEqual(descCell.arrow, [0, 2]); // Right, Bottom
+});
